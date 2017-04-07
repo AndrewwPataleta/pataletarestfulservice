867190c553932f39acb1b4ea40990003516ea307
andrew <andreww.pataleta@gmail.com>
2017-04-07 17:30:44 +0300

    was added one client page menu.html and framework support angular start programming client version

--- src/main/java/com/pataleta/restfullservice/Service/impl/parsStopBy.java
+++ src/main/java/com/pataleta/restfullservice/Service/impl/parsStopBy.java
@@ -1,6 +1,7 @@
 package com.pataleta.restfullservice.Service.impl;
 
 
+
 import com.pataleta.restfullservice.Service.parsWeb;
 import com.pataleta.restfullservice.model.SparepartEntity;
 import org.jsoup.Jsoup;
@@ -13,6 +14,7 @@ import java.util.HashSet;
 
 public class parsStopBy implements parsWeb {
 
+    private String phone = "705-40-05";
     private String basicUrl = "http://stopby.by";
     private HashSet<SparepartEntity> listOfParts = new HashSet<>();
 
@@ -21,24 +23,63 @@ public class parsStopBy implements parsWeb {
         return null;
     }
 
-    private void initListByHref(String urlForSearch) throws IOException {
-        System.out.println(" href: "+urlForSearch);
+        private Element mainDiv(String url){
+            Element mainDivElem = null;
+            try {
+                Document mlAutoDocument = Jsoup.connect(basicUrl + url).timeout(5000).get();
+                mainDivElem = mlAutoDocument.getElementById("ajax_analogs");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return mainDivElem;
+        }
+
+    private Elements getItemsInTableByUrlWithoutAnalogs(String url){
+        Elements items = new Elements();
         try {
-            Document mlAutoDocument = Jsoup.connect(basicUrl + urlForSearch).timeout(5000).get();
-            Element mainTable = mlAutoDocument.getElementById("ajax_analogs");
-            Elements itemsInTable = mainTable.select("table.details-list.filterResultTable.xsmalls").select("tbody").select("tr");
-            for (Element singleItem : itemsInTable) {
+            Element mainTable = mainDiv(url);
+            items = mainTable.select("table.details-list.filterResultTable.xsmalls").select("tbody").first().select("tr");
+        }catch (Exception e){
+            System.out.println(" getItemsInTableWithoutAnalogs свалился ");
+        }
+        return items;
+    }
+
+    private Elements getItemsInTableByUrlWithAnalogs(String url){
+        Elements items = new Elements();
+        try {
+            Element mainTable = mainDiv(url);
+            items = mainTable.select("table.details-list.filterResultTable.xsmalls").select("tbody").select("tr");
+        }catch (Exception e){
+            System.out.println(" getItemsInTableWithAnalogs свалился ");
+        }
+        return items;
+    }
+
+    private void initListByElements(Elements items) throws IOException {
+        try {
+            for (Element singleItem : items) {
                 SparepartEntity item = new SparepartEntity();
                 try {
                     item.setPrice(Float.valueOf(singleItem.getElementsByAttributeValue("itemprop", "price").text()));
+                    if(singleItem.select("a.fancybox-small.droppeda").text().trim().equals(""))
+                        continue;
                     item.setProducer(singleItem.select("a.fancybox-small.droppeda").text());
-                    item.setCharacteristics(singleItem.select("td").select("a").text());
+                    item.setNameSparepart(singleItem.select("td").select("a").text());
+                    item.setPrice(Float.valueOf(singleItem.getElementsByAttributeValue("itemprop", "price")
+                            .text().replace(",",".")));
+                    if(singleItem.select("td.g-brand.cell").text().trim().equals(""))
+                        continue;
+                    item.setProducer(singleItem.select("td.g-brand.cell").text());
+                    item.setNameSparepart(singleItem.select("td.g-descr.cell").select("a").text().replace("Купить",""));
                     item.setDaysOfDelivery(singleItem.select("td.g-delivery.smallprice.cell.t-center").text());
                     item.setQuantity(singleItem.select("td.g-box.cell").text());
                     item.setVendorCode(singleItem.select("td.g-article.cell").text());
+                    item.setPhone(phone);
                     listOfParts.add(item);
+
                 } catch (Exception e) {
-                    System.out.println(" без цены ");
+                    System.out.println(" initList свалился ");
                 }
             }
         }catch (Exception e){
@@ -46,25 +87,106 @@ public class parsStopBy implements parsWeb {
         }
     }
 
+    private Elements getEveryoneBrandByArticle(String article)  {
+        Elements itemsInTable = new Elements();
+        try {
+            Document mlAutoDoc = Jsoup.connect("http://stopby.by/search/number/?article=" + article + "").timeout(5000).get();
+            Element mainTable = mlAutoDoc.getElementById("ajax_analogs");
+            String producer = mainTable.select("table").first().select("tbody").select("tr").select("td").select("h1.uppercase").text();
+            itemsInTable = mainTable.select("table").select("tbody").select("tr.cursor");
+        }catch (Exception e){
+            System.out.println(" getEveryone crash ");
+        }
+        return itemsInTable;
+    }
 
     @Override
     public HashSet<SparepartEntity> getListByArticle(String article) throws IOException {
-            try {
-                Document mlAutoDoc = Jsoup.connect("http://stopby.by/search/number/?article=" + article + "").timeout(5000).get();
-                Element mainTable = mlAutoDoc.getElementById("ajax_analogs");
-                String producer = mainTable.select("table").first().select("tbody").select("tr").select("td").select("h1.uppercase").text();
-                Elements itemsInTable = mainTable.select("table").select("tbody").select("tr.cursor");
-                for (Element singleItem : itemsInTable) {
-                    Element forHref = singleItem.select("td").select("strong").select("a").first();
-                    String href = forHref.attr("href");
-                    initListByHref(href);
-                }
-            }catch (SocketTimeoutException e){
-                System.out.println(" Время ожидания истекло ");
+        try {
+            Elements itemsInTable = getEveryoneBrandByArticle(article);
+            for (Element singleItem : itemsInTable) {
+                Element forHref = singleItem.select("td").select("strong").select("a").first();
+                String href = forHref.attr("href");
+                Elements itemsWithoutAnalogs = getItemsInTableByUrlWithoutAnalogs(href);
+                initListByElements(itemsWithoutAnalogs);
             }
-            catch (NullPointerException e){
-                System.out.println(" Ничего не найдено ");
+        }catch (SocketTimeoutException e){
+            System.out.println(" Время ожидания истекло ");
+        }
+        catch (NullPointerException e){
+            System.out.println(" Ничего не найдено ");
+        }
+        for (SparepartEntity item: listOfParts) {
+            System.out.println(item);
+        }
+        return listOfParts;
+    }
+
+    public HashSet<SparepartEntity> getListByArticleWithAnalogs(String article) throws IOException {
+        try {
+            Elements itemsInTable = getEveryoneBrandByArticle(article);
+            for (Element singleItem : itemsInTable) {
+                Element forHref = singleItem.select("td").select("strong").select("a").first();
+                String href = forHref.attr("href");
+                Elements itemsWithoutAnalogs = getItemsInTableByUrlWithAnalogs(href);
+                initListByElements(itemsWithoutAnalogs);
             }
+        }catch (SocketTimeoutException e){
+            System.out.println(" Время ожидания истекло ");
+        }
+        catch (NullPointerException e){
+            System.out.println(" Ничего не найдено ");
+        }
+        for (SparepartEntity item: listOfParts) {
+            System.out.println(item);
+        }
+        return listOfParts;
+    }
+
+    @Override
+    public HashSet<SparepartEntity> getListByArticle(String article, String brand) throws IOException {
+        try {
+            Elements brands = getEveryoneBrandByArticle(article);
+            for (Element singleItem : brands) {
+                if(!singleItem.select("td").select("strong").select("a").first().text().equals(brand))
+                    continue;
+                Element forHref = singleItem.select("td").select("strong").select("a").first();
+                String href = forHref.attr("href");
+                Elements itemsWithOutAnalogs = getItemsInTableByUrlWithoutAnalogs(href);
+                initListByElements(itemsWithOutAnalogs);
+            }
+        }catch (SocketTimeoutException e){
+            System.out.println(" Время ожидания истекло ");
+        }
+        catch (NullPointerException e){
+            System.out.println(" Ничего не найдено ");
+        }
+        for (SparepartEntity item: listOfParts) {
+            System.out.println(item);
+        }
+        return listOfParts;
+    }
+
+    public HashSet<SparepartEntity> getListByArticleWithAnalogs(String article, String brand) throws IOException {
+        try {
+            Elements brands = getEveryoneBrandByArticle(article);
+            for (Element singleItem : brands) {
+                if(!singleItem.select("td").select("strong").select("a").first().text().equals(brand))
+                    continue;
+                Element forHref = singleItem.select("td").select("strong").select("a").first();
+                String href = forHref.attr("href");
+                Elements itemsWithAnalogs = getItemsInTableByUrlWithAnalogs(href);
+                initListByElements(itemsWithAnalogs);
+            }
+        }catch (SocketTimeoutException e){
+            System.out.println(" Время ожидания истекло ");
+        }
+        catch (NullPointerException e){
+            System.out.println(" Ничего не найдено ");
+        }
+        for (SparepartEntity item: listOfParts) {
+            System.out.println(item);
+        }
         return listOfParts;
     }
 
